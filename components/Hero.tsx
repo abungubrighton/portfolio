1d4c91cd6ae0f3cd1b356a5025dae24d96dc481b
abungubrighton <brighton@ibuqacapital.com>
2023-11-05 15:35:34 +0300

    chore:hero section button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
+import Link from "next/link";
 
 type Props = {}
 
@@ -19,13 +20,27 @@ const Hero = (props: Props) => {
       <div className="h-screen flex flex-col items-center space-y-8 justify-center text-center overflow-hidden">
           <BackgroundCircles />
           <img className="relative rounded-full h-32 w-32 mx-auto object-cover" src="/images/brighton.jpeg" alt="Brighton's image" />
-          <div>
+          <div className="z-20">
               <h2 className="text-sm uppercase pb-2  text-gray-500 tracking-[15px]">Software  Engineer</h2>
             <h1>
                 <span className="text-5xl lg:text-6xl font-semibold px-10">{text}</span>
                 <Cursor cursorColor="#F7AB0A" />
             </h1>
               
+              <div className="pt-5">
+                  <Link href="#about">
+                    <button className="heroButton">About</button>
+                  </Link>
+                  <Link href="#experience">
+                    <button className="heroButton">Experience</button>
+                  </Link>
+                  <Link href="#skills">
+                    <button className="heroButton">Skills</button>
+                  </Link>
+                  <Link href="#projects">
+                    <button className="heroButton">Projects</button>
+                  </Link>
+            </div>
           </div>
       </div>
   )
