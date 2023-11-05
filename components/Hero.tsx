205168819b55189826e0070ddce129164c4d8b61
abungubrighton <111748970+abungubrighton@users.noreply.github.com>
2023-11-05 15:36:43 +0300

    chore:hero image (#6)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,15 +11,22 @@ const Hero = (props: Props) => {
             "Mobile Developer",
             "Data Viz Enthusiast",
             "Machine Learning Enthusiast",
-        ]
+        ],
+        loop: true,
+        delaySpeed:2000,
     })
   return (
-      <div>
+      <div className="h-screen flex flex-col items-center space-y-8 justify-center text-center overflow-hidden">
           <BackgroundCircles />
-          <h1>
-              <span>{text}</span>
-              <Cursor cursorColor="#F7AB0A" />
-        </h1>
+          <img className="relative rounded-full h-32 w-32 mx-auto object-cover" src="/images/brighton.jpeg" alt="Brighton's image" />
+          <div>
+              <h2 className="text-sm uppercase pb-2  text-gray-500 tracking-[15px]">Software  Engineer</h2>
+            <h1>
+                <span className="text-5xl lg:text-6xl font-semibold px-10">{text}</span>
+                <Cursor cursorColor="#F7AB0A" />
+            </h1>
+              
+          </div>
       </div>
   )
 }
