198f05970a09c38dac773f8dd0ed8aa637daeb18
abungubrighton <111748970+abungubrighton@users.noreply.github.com>
2023-11-06 08:48:20 +0300

    chore:hero section button (#7)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,16 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen z-0 overflow-scroll snap-y snap-mandatory' >
+      <Head>
+        <title>Brighton's Portfolio</title>
+      </Head>
       {/* Header */}
       <Header />
       {/* Hero */}
