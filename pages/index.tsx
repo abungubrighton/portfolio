9323009d2ce380de44dca3224725e8d605c5e206
abungubrighton <111748970+abungubrighton@users.noreply.github.com>
2023-11-08 08:41:28 +0300

    chore:about section (#8)

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Head from 'next/head'
+import About from '@/components/About'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,10 +16,13 @@ export default function Home() {
       {/* Header */}
       <Header />
       {/* Hero */}
-      <section id="hero" className='snap-center'>
+      <section id="hero" className='snap-start'>
         <Hero />
       </section>
       {/* About */}
+      <section id="about" className='snap-center'>
+        <About />
+      </section>
       {/* Experience */}
       {/* Skills */}
       {/* Projects */}
