66161157980a8578282d2f4045bc4619a290a8e4
abungubrighton <111748970+abungubrighton@users.noreply.github.com>
2023-11-12 20:14:00 +0300

    chore:projects components (#11)

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Head from 'next/head'
 import About from '@/components/About'
 import WorkExperience from '@/components/WorkExperience'
 import Skills from '@/components/Skills'
+import Projects from '@/components/Projects'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -34,6 +35,9 @@ export default function Home() {
         <Skills/>
       </section>
       {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
       {/* Contact Me */}
     </div>
   )
