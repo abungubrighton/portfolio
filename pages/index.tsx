485a04ded1c734833ccfa09f8af4f1e7de9a092c
abungubrighton <111748970+abungubrighton@users.noreply.github.com>
2023-12-24 19:26:36 +0300

    chore:customize scrollbars (#15)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,56 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import Head from 'next/head'
-import About from '@/components/About'
-import WorkExperience from '@/components/WorkExperience'
-import Skills from '@/components/Skills'
-import Projects from '@/components/Projects'
-import Contactme from '@/components/Contactme'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Head from "next/head";
+import About from "@/components/About";
+import WorkExperience from "@/components/WorkExperience";
+import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
+import Contactme from "@/components/Contactme";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen z-0 overflow-scroll snap-y snap-mandatory' >
+    <div className="bg-[rgb(36,36,36)] text-white h-screen z-0 overflow-y-scroll overflow-x-hidden snap-y snap-mandatory scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
         <title>Brighton's Portfolio</title>
       </Head>
       {/* Header */}
       <Header />
       {/* Hero */}
-      <section id="hero" className='snap-center'>
+      <section id="hero" className="snap-center">
         <Hero />
       </section>
       {/* About */}
-      <section id="about" className='snap-center'>
+      <section id="about" className="snap-center">
         <About />
       </section>
       {/* Experience */}
-      <section id="experienc" className='snap-center'>
+      <section id="experienc" className="snap-center">
         <WorkExperience />
       </section>
       {/* Skills */}
-      <section id="skills" className='snap-start'>
-        <Skills/>
+      <section id="skills" className="snap-start">
+        <Skills />
       </section>
       {/* Projects */}
       <section id="projects" className="snap-start">
         <Projects />
       </section>
       {/* Contact Me */}
-      <section id='contact' className='snap-start'>
+      <section id="contact" className="snap-start">
         <Contactme />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5  cursor-pointer">
+          <div className="flex items-center justify-center ">
+            <img className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer" src="/images/top.png" alt="image of arrow pointing up" /></div>
+        </footer>
+      </Link>
     </div>
-  )
+  );
 }
