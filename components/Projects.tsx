736a7eedf7e1f90228a4700c27bab5cbe4f7d1d2
abungubrighton <brighton@ibuqacapital.com>
2023-12-26 10:46:21 +0300

    chore:update project copy

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,8 +1,9 @@
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
+import { projects } from "../data";
+import Link from "next/link";
 type Props = {};
 
 function Projects({}: Props) {
-  const projects = [1, 2, 3, 4, 5];
   return (
     <div className="relative h-screen flex flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
       <h3 className="absolute top-24 uppercase tracking-[20px]  text-gray-500  text-2xl">
@@ -11,35 +12,40 @@ function Projects({}: Props) {
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
         {/* Each of the projects being mapped out */}
         {projects.map((project, i) => (
-            <motion.div
-                initial={{
-                    y: -300,
-                    opacity: 0
-                }}
-                transition={{ duration: 1.2 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                viewport={{once: true}}
-                className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p44 h-screen ">
+          <motion.div
+            initial={{
+              y: -300,
+              opacity: 0,
+            }}
+            transition={{ duration: 1.2 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p44 h-screen "
+          >
             <img
-              src="/projects/hulu-clone.png"
+              src={project.projectImageUrl}
               alt=""
               className="w-full h-1/2 object-contain"
             />
-            <div className="space-y-10 px-0 md:x-10 max-w-6xl">
+            <div className="space-y-5 px-0 md:x-10 max-w-6xl">
               <h4 className="text-4xl font-semibold text-center">
                 <span className="underline mr-1 decoration-[#F7AB0A]/50">
                   Case study {i + 1} of {projects.length}:
                 </span>
-                Hulu clone
+                {project.projectName}
+                <div className="p-0 m-0">
+                  <Link
+                    className="text-center text-sm underline"
+                    href={project.projectLink}
+                    target="_blank"
+                  >
+                    project link
+                  </Link>
+                </div>
               </h4>
               {/* project summary */}
-              <p className="text-lg text-center md:text-left ">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa,
-                placeat voluptas rem quos earum quibusdam adipisci nam quasi
-                laborum soluta quisquam sunt porro ex accusantium officiis
-                nesciunt ratione. Numquam, illum! Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Et quia quidem eaque commodi
-                repellat dolorum voluptatibus consequuntur sint sunt obcaecati!
+              <p className="text-lg text-center md:text-left m-0 p-0 ">
+                {project.projectSummary}
               </p>
             </div>
           </motion.div>
---
+++ data.js
@@ -0,0 +1,30 @@
+export const projects = [{
+    projectName: "Netflix Clone",
+    projectSummary: `
+    A Hulu-inspired streaming platform,
+    leveraging technologies such as Netflix and TMDB APIs to enhance content curation and user experience.
+    Key highlights of the project include:API Integration Expertise,Dynamic Content Aggregation,User-Centric Design and Cross-Platform Compatibility
+    `,
+    projectLink: 'https://github.com/abungubrighton/netflix-react-clone',
+    projectImageUrl: "/projects/netflix-clone.png"
+}, {
+    projectName: "Hulu Clone",
+    projectSummary: `
+    A Hulu-inspired streaming platform,
+    leveraging technologies such as Netflix and TMDB APIs to enhance content curation and user experience.
+    Key highlights of the project include:API Integration Expertise,Dynamic Content Aggregation,User-Centric Design and Cross-Platform Compatibility
+    `,
+    projectLink: 'https://hulu-nextjs-clone-bwi62ssqq-brightonotienos-projects.vercel.app',
+    projectImageUrl: "/projects/hulu-clone.png"
+}, {
+    projectName: "Tesla Clone",
+    projectSummary: `
+    A Tesla website clone, replicating the design and functionality of the official Tesla website.
+    leveraging cutting-edge  frontend technologies including ReactJS, Styled Components, Material-UI, and Redux.
+     Key achievements and contributions encompass: ReactJS Mastery, Styled Components Implementation,Material-UI Integration and Redux State Management
+     
+    
+    `,
+    projectLink: 'https://github.com/abungubrighton/tesla-site-clone-react',
+    projectImageUrl: "/projects/tesla-clone.png"
+}, ]
