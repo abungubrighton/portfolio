0c90f0cf6b186ceb1af75f53364dec757e701b40
abungubrighton <brighton@ibuqacapital.com>
2024-10-30 08:55:39 +0300

    chore:Add the iphone site clone to projects

--- data.js
+++ data.js
@@ -1,13 +1,24 @@
-export const projects = [{
+export const projects = [
+  {
+    projectName: "Apple Clone",
+    projectSummary: `
+    Imagine experiencing the sleek, immersive beauty of Apple's iPhone 15 Pro website but reimagined from the ground up! This clone was crafted using cutting-edge tech—React.js and TailwindCSS—for a lightning-fast, visually stunning, and mobile-responsive experience. With powerful, seamless animations powered by GSAP and breathtaking 3D models rendered with Three.js, users can explore the iPhone 15 Pro in various colors and forms like never before. It’s a showstopper designed to bring premium, Apple-like allure to any product showcase.
+    `,
+    projectLink: "https://brighton-apple-website-clone.vercel.app/",
+    projectImageUrl: "/projects/apple-site-clone.png",
+  },
+  {
     projectName: "Hulu Clone",
     projectSummary: `
     A Hulu-inspired streaming platform,
     leveraging technologies such as ReactJs , TMDB APIs and Firebase to enhance content curation and user experience.
     Key highlights of the project include:API Integration Expertise,Dynamic Content Aggregation,User-Centric Design and Cross-Platform Compatibility
     `,
-    projectLink: 'https://hulu-nextjs-clone-bwi62ssqq-brightonotienos-projects.vercel.app',
-    projectImageUrl: "/projects/hulu-clone.png"
-}, {
+    projectLink:
+      "https://hulu-nextjs-clone-bwi62ssqq-brightonotienos-projects.vercel.app",
+    projectImageUrl: "/projects/hulu-clone.png",
+  },
+  {
     projectName: "Ecommerce Platform",
     projectSummary: `
     A feature-rich e-commerce platform encompassing:
@@ -16,9 +27,10 @@ export const projects = [{
     Efficient Admin Tools: Streamlined admin controls for product, user, and order management.
     Smooth Checkout Process: Integrated PayPal/credit card payments and streamlined checkout with shipping options.
     `,
-    projectLink: 'https://github.com/abungubrighton/shopout',
-    projectImageUrl: "/projects/shopout.png"
-}, {
+    projectLink: "https://github.com/abungubrighton/shopout",
+    projectImageUrl: "/projects/shopout.png",
+  },
+  {
     projectName: "Netflix Clone",
     projectSummary: `
     A Netflix-inspired streaming platform built with React and Redux pulls content from the TMDB API,
@@ -26,9 +38,10 @@ export const projects = [{
      React provides a responsive, dynamic user interface, while Redux manages global state, keeping user sessions, content data,
       and subscription info in sync across the platform. With this setup, users enjoy a seamless, personalized streaming experience across devices.
     `,
-    projectLink: 'https://brighton-netflix-app-clone.vercel.app/',
-    projectImageUrl: "/projects/netflix-clone.png"
-}, {
+    projectLink: "https://brighton-netflix-app-clone.vercel.app/",
+    projectImageUrl: "/projects/netflix-clone.png",
+  },
+  {
     projectName: "Tesla Clone",
     projectSummary: `
     A Tesla website clone, replicating the design and functionality of the official Tesla website.
@@ -37,48 +50,49 @@ export const projects = [{
      
     
     `,
-    projectLink: 'https://github.com/abungubrighton/tesla-site-clone-react',
-    projectImageUrl: "/projects/tesla-clone.png"
-}, ]
+    projectLink: "https://github.com/abungubrighton/tesla-site-clone-react",
+    projectImageUrl: "/projects/tesla-clone.png",
+  },
+];
 
-
-export const experiences = [{
-    companyLogoUrl: '/logos/acw.png',
-    companyName: 'Africa Code Week',
+export const experiences = [
+  {
+    companyLogoUrl: "/logos/acw.png",
+    companyName: "Africa Code Week",
     position: "Lead Skills Trainer",
     mainResponsibility: `
     Trained Teachers and Student from both Nairobi and Kisumu Counties
     under the Young Science Society of Kenya program
     `,
     responsibilities: [
-        'programming skills: HTML,javascript, css and scratch programming,',
-        'pitching and entrepreneurial skills'
+      "programming skills: HTML,javascript, css and scratch programming,",
+      "pitching and entrepreneurial skills",
     ],
     technologies: [
-        '/tech/scratch.png',
-        '/tech/html.png',
-        '/tech/js.png',
-        '/tech/css.png',
+      "/tech/scratch.png",
+      "/tech/html.png",
+      "/tech/js.png",
+      "/tech/css.png",
     ],
-
-}, {
-    companyLogoUrl: '/logos/ibuqa.jpeg',
-    companyName: 'IbuQa Capital',
+  },
+  {
+    companyLogoUrl: "/logos/ibuqa.jpeg",
+    companyName: "IbuQa Capital",
     position: "Software Engineer",
     mainResponsibility: `
         Full stack software development
     `,
     responsibilities: [
-        `Development and Implementation: Building and implementing scalable, high-quality software solutions across the entire technology stack.`,
-        `Database Management: Creating and managing databases, ensuring data integrity, and optimizing database performance`,
-        `User Interface/User Experience (UI/UX) Design`
+      `Development and Implementation: Building and implementing scalable, high-quality software solutions across the entire technology stack.`,
+      `Database Management: Creating and managing databases, ensuring data integrity, and optimizing database performance`,
+      `User Interface/User Experience (UI/UX) Design`,
     ],
     technologies: [
-        '/tech/django.png',
-        '/tech/react.png',
-        '/tech/postgres.png',
-        '/tech/docker.jpeg',
-        '/tech/kubernetes.png',
+      "/tech/django.png",
+      "/tech/react.png",
+      "/tech/postgres.png",
+      "/tech/docker.jpeg",
+      "/tech/kubernetes.png",
     ],
-
-}, ]
+  },
+];
